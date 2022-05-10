bea3447286b2bf7071f978b5de01eede749d0188
rotacloud-johnney <johnney@rotacloud.com>
2022-05-10 21:03:32 +0800

    Added more properties for the leave model

--- src/models/leave.model.ts
+++ src/models/leave.model.ts
@@ -12,6 +12,17 @@ export class Leave {
   public requested: boolean;
   public admin_message: string | null;
   public user_message: string | null;
+  public deleted: boolean;
+  public deleted_at: number | null;
+  public deleted_by: number | null;
+  public admin: number;
+  public start_am_pm: string;
+  public end_am_pm: string;
+  public hours: { [key: string]: number };
+  public hours_method: string;
+  public hours_set: boolean;
+  public requested_at: number;
+  public replied_at: number;
 
   constructor(leave: ApiLeave) {
     this.id = leave.id;
@@ -24,5 +35,15 @@ export class Leave {
     this.requested = leave.requested;
     this.admin_message = leave.admin_message;
     this.user_message = leave.user_message;
+    this.deleted_at = leave.deleted_at;
+    this.deleted_by = leave.deleted_by;
+    this.admin = leave.admin;
+    this.start_am_pm = leave.start_am_pm;
+    this.end_am_pm = leave.end_am_pm;
+    this.hours = leave.hours;
+    this.hours_method = leave.hours_method;
+    this.hours_set = leave.hours_set;
+    this.requested_at = leave.requested_at;
+    this.replied_at = leave.replied_at;
   }
 }
