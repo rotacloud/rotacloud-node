cc5818384214588c5f98c4eb0be8d202aca16060
rotacloud-johnney <johnney@rotacloud.com>
2022-05-10 16:18:56 +0800

    Added more properties in leave model

--- src/models/leave.model.ts
+++ src/models/leave.model.ts
@@ -2,10 +2,27 @@ import { ApiLeave, ApiLeaveDate } from '../interfaces/index.js';
 
 export class Leave {
   public id: number;
+  public type: number;
+  public user: number;
+  public start_date: string;
+  public end_date: string;
   public dates: ApiLeaveDate[];
+  public paid: boolean;
+  public status: string;
+  public requested: boolean;
+  public admin_message: string;
+  public user_message: string;
 
   constructor(leave: ApiLeave) {
     this.id = leave.id;
     this.dates = leave.dates;
+    this.user = leave.user;
+    this.start_date = leave.start_date;
+    this.end_date = leave.end_date;
+    this.paid = leave.paid;
+    this.status = leave.status;
+    this.requested = leave.requested;
+    this.admin_message = leave.admin_message;
+    this.user_message = leave.user_message;
   }
 }
