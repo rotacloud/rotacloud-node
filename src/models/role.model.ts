859c81af93919bb87eda7aa88252454c1511ad85
richwatts0 <rich@rotacloud.com>
2022-05-11 16:02:18 +0100

    Added users property on roles and groups models

--- src/models/role.model.ts
+++ src/models/role.model.ts
@@ -4,8 +4,11 @@ export class Role {
   public id: number;
   public name: string;
   public colour: string;
+  public users: number[];
+
   constructor(role: ApiRole) {
     this.id = role.id;
     this.name = role.name;
+    this.users = role.users;
   }
 }
