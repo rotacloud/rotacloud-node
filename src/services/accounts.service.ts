3e21ead53501772d8629772cbe43132e359f8201
richwatts0 <rich@rotacloud.com>
2022-08-02 11:40:59 +0100

    Added additional accounts service method

--- src/services/accounts.service.ts
+++ src/services/accounts.service.ts
@@ -1,10 +1,22 @@
+import { AxiosResponse } from 'axios';
 import { Account } from '../models/account.model.js';
 import { Service, Options } from './index.js';
 
 import { ApiAccount } from '../interfaces/index.js';
+import { ErrorResponse } from '../models/error-response.model.js';
 
 class AccountsService extends Service {
   private apiPath = '/accounts';
+
+  get(id: number): Promise<Account>;
+  get(id: number, options: { rawResponse: true } & Options): Promise<AxiosResponse<ApiAccount, any>>;
+  get(id: number, options: Options): Promise<Account>;
+  get(id: number, options?: Options) {
+    return super.fetch<ApiAccount>({ url: `${this.apiPath}/${id}` }, options).then(
+      (res) => Promise.resolve(options?.rawResponse ? res : new Account(res.data)),
+      (err) => Promise.reject(options?.rawResponse ? err : new ErrorResponse(err))
+    );
+  }
 
   async *list(options?: Options) {
     for await (const res of super.iterator<ApiAccount>({ url: this.apiPath }, options)) {
