f74052c04ddb4d729a1b6d74db75da8267b69d72
Rich Watts <91881876+richwatts0@users.noreply.github.com>
2023-03-15 13:12:43 +0000

    Develop -> Main (#62)
    * Merge pull request #50 from rotacloud/develop (#52)
    
    Co-authored-by: HeyJonR <52401813+HeyJonR@users.noreply.github.com>
    
    * wip leave models
    
    * version bump
    
    * Added visible_employees in day note
    
    * reverted package-lock json changes
    
    * version bump
    
    * feat: ability to specify extra headers on requests (#56)
    
    * added a new parameter for user endpoint
    
    * v bump
    
    ---------
    
    Co-authored-by: HeyJonR <52401813+HeyJonR@users.noreply.github.com>
    Co-authored-by: Johnney Dela Cruz <johnney@rotacloud.com>
    Co-authored-by: Leonard Aguanta <leo@rotacloud.com>
    Co-authored-by: Leonard Aguanta <107027759+leonardrota@users.noreply.github.com>
    Co-authored-by: rotacloud-johnney <93248739+rotacloud-johnney@users.noreply.github.com>
    Co-authored-by: Liam <91461742+liammoore4@users.noreply.github.com>

--- src/interfaces/query-params/users-query-params.interface.ts
+++ src/interfaces/query-params/users-query-params.interface.ts
@@ -1,6 +1,14 @@
 export interface UsersQueryParams {
   ids?: number[];
   locations?: number[];
+  /**
+   * Works different from {UserQueryParams.locations},
+   * When filtering out employees for rota, this is more suggested,
+   * one scenario would be filtering out employees base on their starting and
+   * final working date, using {UserQueryParams.locations} will not work in tandem
+   * of {UserQueryParams.start_date} and {UserQueryParams.end_date}
+   */
+  location?: number;
   roles?: number[];
   only_managed?: boolean;
   include_self?: boolean;
