cdd194addb991520fd7d0b81f3071074c1bacbe5
Harlan <harlan@rotacloud.com>
2023-04-12 14:48:52 +0100

    re-add optional chaining

--- src/models/SDKError.model.ts
+++ src/models/SDKError.model.ts
@@ -11,7 +11,8 @@ export class SDKError extends Error {
   constructor(errorConfig: SDKErrorConfig) {
     super(errorConfig.message);
 
-    Error.captureStackTrace(this, SDKError);
+    Error.captureStackTrace?.(this, SDKError);
+    // optional chaining needed as is v8 specific https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error#static_methods
 
     this.data = errorConfig.data;
     this.code = errorConfig.code;
