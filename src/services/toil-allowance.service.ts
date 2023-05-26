3556dd0a11969801e23a8f1e38e937a43bf369ed
Harlan <harlan@rotacloud.com>
2023-05-26 13:42:31 +0100

    update year in path for toilAllowance

--- src/services/toil-allowance.service.ts
+++ src/services/toil-allowance.service.ts
@@ -6,22 +6,25 @@ import { ToilAllowance } from '../models/toil-allowance.model';
 export class ToilAllowanceService extends Service {
   private apiPath = '/toil_allowance';
 
-  async *list(query: ToilAllowanceQueryParams, options?: Options) {
-    for await (const res of super.iterator<ApiToilAllowance>({ url: this.apiPath, params: query }, options)) {
+  async *list(year: number, query: ToilAllowanceQueryParams, options?: Options) {
+    for await (const res of super.iterator<ApiToilAllowance>(
+      { url: `${this.apiPath}/${year}`, params: query },
+      options
+    )) {
       yield new ToilAllowance(res);
     }
   }
 
-  listAll(query: ToilAllowanceQueryParams, options?: Options): Promise<ToilAllowance[]>;
-  async listAll(query: ToilAllowanceQueryParams, options?: Options) {
+  listAll(year: number, query: ToilAllowanceQueryParams, options?: Options): Promise<ToilAllowance[]>;
+  async listAll(year: number, query: ToilAllowanceQueryParams, options?: Options) {
     const toilAllowances = [] as ToilAllowance[];
-    for await (const allowance of this.list(query, options)) {
+    for await (const allowance of this.list(year, query, options)) {
       toilAllowances.push(allowance);
     }
     return toilAllowances;
   }
 
-  listByPage(query: ToilAllowanceQueryParams, options?: Options) {
-    return super.iterator<ApiToilAllowance>({ url: this.apiPath, params: query }, options).byPage();
+  listByPage(year: number, query: ToilAllowanceQueryParams, options?: Options) {
+    return super.iterator<ApiToilAllowance[]>({ url: `${this.apiPath}/${year}`, params: query }, options).byPage();
   }
 }
