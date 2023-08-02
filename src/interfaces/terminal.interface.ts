850b93f476238abded5515917f54fcb532f2bdfa
Richard Watts <richmwatts@macmini.local>
2023-08-02 11:54:44 +0100

    Corrected types

--- src/interfaces/terminal.interface.ts
+++ src/interfaces/terminal.interface.ts
@@ -7,11 +7,18 @@ export interface ApiTerminal {
   device: string | null;
   version: string | null;
   ip: string | null;
-  location: any | null; // todo
-  timezone: number;
+  location: ApiTerminalLocation | null;
+  timezone: number | ExpandedTimeZone;
   require_photo: boolean;
   require_photo_breaks: boolean;
   debug: boolean;
+}
+
+export interface ExpandedTimeZone {
+  id: number;
+  name: string;
+  city: string;
+  sub: string | null;
 }
 
 export interface ApiTerminalLocation {
