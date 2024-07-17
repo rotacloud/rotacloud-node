e198e1ea5116556878da8b75208eda386e0ab6fc
Leonard Aguanta <107027759+leonardrota@users.noreply.github.com>
2024-07-17 16:40:52 +0800

    bug/RWA-1809 Update role colour to support null values (#140)
    * Update role colour to support null values
    
    * change to color

--- src/interfaces/role.interface.ts
+++ src/interfaces/role.interface.ts
@@ -2,7 +2,7 @@ export interface Role {
   id: number;
   deleted: boolean;
   name: string;
-  colour: string;
+  colour: string | null;
   default_break: number;
   users: number[];
   pay_code: string;
