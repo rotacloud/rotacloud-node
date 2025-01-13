7d658c232240bbe29f851a3456d5ade61b6bf797
liammoore4 <liam@rotacloud.com>
2025-01-13 15:42:40 +0000

    fix: type fixes for ops and easier to use OpDef

--- src/ops.ts
+++ src/ops.ts
@@ -1,7 +1,7 @@
 import { Axios, AxiosRequestConfig, AxiosResponse } from 'axios';
 import assert from 'assert';
 import { ServiceSpecification } from './service.js';
-import { RequestOptions, QueryParameterValue } from './utils.js';
+import { RequestOptions, QueryParameterValue, RequirementsOf } from './utils.js';
 import { Endpoint, EndpointVersion } from './endpoint.js';
 
 // TODO: should we add "upsert"?
@@ -10,7 +10,7 @@ export type Operation = 'get' | 'list' | 'listAll' | 'delete' | 'create' | 'upda
 /** Context provided to all operations */
 export type OperationContext = {
   client: Readonly<Axios>;
-  request: Readonly<AxiosRequestConfig<unknown>>;
+  request: Readonly<AxiosRequestConfig>;
   service: Readonly<ServiceSpecification>;
 };
 
@@ -28,101 +28,100 @@ export type RequestConfig<RequestData, ResponseData> = AxiosRequestConfig<Reques
  * This is intended as a convenient way of defining {@see OpFunction}s that can then be
  * "built" ready for the end user to call ({@see buildOp})
  * */
-export type OpDef<T, P = any, O extends Partial<RequestOptions<any>> = RequestOptions<unknown>, R = T> =
-  | ((ctx: OperationContext, param: P, opts?: O) => RequestConfig<T, R>)
-  | ((ctx: OperationContext, param: P, opts?: O) => AsyncIterable<T>)
-  | ((ctx: OperationContext, param: P, opts?: O) => Promise<T>);
+export type OpDef<T, Param = any, Opts extends Partial<RequestOptions<any>> = RequestOptions<unknown>, Return = T> =
+  | ((ctx: OperationContext, param: Param, opts?: Opts) => RequestConfig<T, Return>)
+  | ((ctx: OperationContext, param: Param, opts?: Opts) => AsyncIterable<T>)
+  | ((ctx: OperationContext, param: Param, opts?: Opts) => Promise<T>);
 
 /** Operation function type to be called by the end user of the SDK.
  *
  * All methods on services follow this typing
  * */
-export type OpFunction<R = any, Param = undefined> =
-  R extends AsyncIterable<infer U>
-    ? // List based op parameter names
-      Param extends undefined
+export type OpFunction<R = any, Param = undefined> = R extends AsyncIterable<infer U> | Promise<Iterable<infer U>>
+  ? // List based op parameter names
+    Param extends undefined
+    ? {
+        (query?: Param): R;
+        <F extends keyof U>(
+          query: Param,
+          options: { fields: F[] } & RequestOptions<U>,
+        ): Promise<AsyncIterable<Pick<U, F>>>;
+        (query: Param, opts?: RequestOptions<U>): R;
+      }
+    : Partial<Param> extends Param
       ? {
           (query?: Param): R;
           <F extends keyof U>(
             query: Param,
             options: { fields: F[] } & RequestOptions<U>,
           ): Promise<AsyncIterable<Pick<U, F>>>;
+          (query?: Param, opts?: RequestOptions<U>): R;
+        }
+      : {
+          (query: Param): R;
+          <F extends keyof U>(
+            query: Param,
+            options: { fields: F[] } & RequestOptions<U>,
+          ): Promise<AsyncIterable<Pick<U, F>>>;
           (query: Param, opts?: RequestOptions<U>): R;
         }
-      : Partial<Param> extends Param
-        ? {
-            (query?: Param): R;
-            <F extends keyof U>(
-              query: Param,
-              options: { fields: F[] } & RequestOptions<U>,
-            ): Promise<AsyncIterable<Pick<U, F>>>;
-            (query?: Param, opts?: RequestOptions<U>): R;
-          }
-        : {
-            (query: Param): R;
-            <F extends keyof U>(
-              query: Param,
-              options: { fields: F[] } & RequestOptions<U>,
-            ): Promise<AsyncIterable<Pick<U, F>>>;
-            (query: Param, opts?: RequestOptions<U>): R;
-          }
-    : Param extends number
+  : Param extends number
+    ? {
+        (id: Param): R;
+        <F extends keyof Awaited<R>>(
+          id: Param,
+          options: { fields: F[]; rawResponse: true } & RequestOptions<Awaited<R>>,
+        ): Promise<AxiosResponse<Pick<Awaited<R>, F>>>;
+        <F extends keyof Awaited<R>>(
+          id: Param,
+          options: { fields: F[] } & RequestOptions<Awaited<R>>,
+        ): Promise<Pick<Awaited<R>, F>>;
+        (
+          id: Param,
+          options?: {
+            rawResponse: true;
+          } & RequestOptions<Awaited<R>>,
+        ): Promise<AxiosResponse<Awaited<R>>>;
+        (id: Param, options?: RequestOptions<Awaited<R>>): R;
+      }
+    : Param extends undefined
       ? {
-          (id: Param): R;
+          (entity?: Param): R;
           <F extends keyof Awaited<R>>(
-            id: Param,
+            entity: Param,
             options: { fields: F[]; rawResponse: true } & RequestOptions<Awaited<R>>,
           ): Promise<AxiosResponse<Pick<Awaited<R>, F>>>;
           <F extends keyof Awaited<R>>(
-            id: Param,
+            entity: Param,
             options: { fields: F[] } & RequestOptions<Awaited<R>>,
           ): Promise<Pick<Awaited<R>, F>>;
           (
-            id: Param,
+            entity?: Param,
             options?: {
               rawResponse: true;
             } & RequestOptions<Awaited<R>>,
           ): Promise<AxiosResponse<Awaited<R>>>;
-          (id: Param, options?: RequestOptions<Awaited<R>>): R;
+          (entity?: Param, options?: RequestOptions<Awaited<R>>): R;
         }
-      : Param extends undefined
-        ? {
-            (entity?: Param): R;
-            <F extends keyof Awaited<R>>(
-              entity: Param,
-              options: { fields: F[]; rawResponse: true } & RequestOptions<Awaited<R>>,
-            ): Promise<AxiosResponse<Pick<Awaited<R>, F>>>;
-            <F extends keyof Awaited<R>>(
-              entity: Param,
-              options: { fields: F[] } & RequestOptions<Awaited<R>>,
-            ): Promise<Pick<Awaited<R>, F>>;
-            (
-              entity?: Param,
-              options?: {
-                rawResponse: true;
-              } & RequestOptions<Awaited<R>>,
-            ): Promise<AxiosResponse<Awaited<R>>>;
-            (entity?: Param, options?: RequestOptions<Awaited<R>>): R;
-          }
-        : // Entity based op parameter names
-          {
-            (entity: Param): R;
-            <F extends keyof R>(
-              entity: Param,
-              options: { fields: F[]; rawResponse: true } & RequestOptions<Awaited<R>>,
-            ): Promise<AxiosResponse<Pick<Awaited<R>, F>>>;
-            <F extends keyof R>(
-              entity: Param,
-              options: { fields: F[] } & RequestOptions<Awaited<R>>,
-            ): Promise<Pick<Awaited<R>, F>>;
-            (
-              entity: Param,
-              options?: {
-                rawResponse: true;
-              } & RequestOptions<Awaited<R>>,
-            ): Promise<AxiosResponse<Awaited<R>>>;
-            (entity: Param, options?: RequestOptions<Awaited<R>>): R;
-          };
+      : // Entity based op parameter names
+        {
+          (entity: Param): R;
+          <F extends keyof Awaited<R>>(
+            entity: Param,
+            options: { fields: F[]; rawResponse: true } & RequestOptions<Awaited<R>>,
+          ): Promise<AxiosResponse<Pick<Awaited<R>, F>>>;
+          <F extends keyof Awaited<R>>(
+            entity: Param,
+            options: { fields: F[] } & RequestOptions<Awaited<R>>,
+          ): Promise<Pick<Awaited<R>, F>>;
+          (
+            entity: Param,
+            options?: {
+              rawResponse: true;
+            } & RequestOptions<Awaited<R>>,
+          ): Promise<AxiosResponse<Awaited<R>>>;
+          (entity: Param, options?: RequestOptions<Awaited<R>>): R;
+        };
 
 /** Utility for creating a query params map needed by most API requests */
 export function paramsFromOptions<T>(opts: RequestOptions<T>): Record<string, QueryParameterValue> {
@@ -182,7 +181,7 @@ function createOp<T = unknown, NewEntity = unknown>(
 }
 
 /** Operation for updating an entity */
-function updateOp<Return, Entity extends { id: number } & Return>(
+function updateOp<Return, Entity extends { id: number } & Partial<Return>>(
   ctx: OperationContext,
   entity: Entity,
 ): RequestConfig<Entity, Return> {
@@ -298,15 +297,15 @@ export function getOpMap<E extends Endpoint<any, any>, T extends E['type'] = E['
       list: listOp<T, E['queryParameters']>,
       listAll: listAllOp<T, E['queryParameters']>,
       create: createOp<T, E['createType']>,
-      update: updateOp<T, T extends { id: number } ? T : never>,
+      update: updateOp<T, T extends { id: number } ? RequirementsOf<T, 'id'> : never>,
     },
     v2: {
       get: getOp<T>,
       delete: deleteOp,
       list: listOp<T, E['queryParameters']>,
-      listAll: listAllOp<E['queryParameters'], T>,
+      listAll: listAllOp<T, E['queryParameters']>,
       create: createOp<T, E['createType']>,
-      update: updateOp<T, T extends { id: number } ? T : never>,
+      update: updateOp<T, T extends { id: number } ? RequirementsOf<T, 'id'> : never>,
     },
   } satisfies Record<EndpointVersion, Record<Operation, OpDef<any, any>>>;
 }
