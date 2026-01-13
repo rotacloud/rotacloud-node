3208837303bf36246527732590daee657ffcfaca
Josh <josh@rotacloud.com>
2026-01-13 15:21:19 +0000

    update nationalInsuranceNumber to be an optional property to match the backend dto

--- src/interfaces/onboarding.interface.ts
+++ src/interfaces/onboarding.interface.ts
@@ -5,7 +5,7 @@ export interface UpdateUserWithOnboardingInfo {
   title: string;
   gender: string;
   dob: string;
-  nationalInsuranceNumber: string;
+  nationalInsuranceNumber?: string;
   address1: string;
   address2: string;
   county: string;
